904bb4c1c3ca157bbf8a3ce2b02336bcf42b8a1b
rimangulov <rimangulov@phoenixit.ru>
2022-05-23 23:59:56 +0500

    encapsulating methods from EmployeeEndpoint to EmployeeService

--- complete/src/main/java/com/example/producingwebservice/endpoint/EmployeeEndpoint.java
+++ complete/src/main/java/com/example/producingwebservice/endpoint/EmployeeEndpoint.java
@@ -1,122 +1,47 @@
 package com.example.producingwebservice.endpoint;
 
-import com.example.producingwebservice.domain.Employee;
-import com.example.producingwebservice.domain.EmployeePosition;
-import com.example.producingwebservice.mapper.EmployeeMapper;
-import com.example.producingwebservice.repository.EmployeeRepository;
+import com.example.producingwebservice.service.EmployeeService;
 import https.www_rob_com.gen.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import java.util.Optional;
-
 @Endpoint
+@RequiredArgsConstructor
 public class EmployeeEndpoint {
     private static final String NAMESPACE_URI = "https://www.rob.com/gen";
 
-    private final EmployeeRepository employeeRepository;
-    private final EmployeeMapper employeeMapper;
-
-    public EmployeeEndpoint(EmployeeRepository employeeRepository, EmployeeMapper employeeMapper) {
-        this.employeeRepository = employeeRepository;
-        this.employeeMapper = employeeMapper;
-    }
-
+    private final EmployeeService employeeService;
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetEmployeeDetailsRequest")
     @ResponsePayload
     public GetEmployeeDetailsResponse getEmployee(@RequestPayload GetEmployeeDetailsRequest request) {
-        GetEmployeeDetailsResponse response = new GetEmployeeDetailsResponse();
-        response.setEmployeeDetails(employeeMapper.mapToEmployeeDetails(employeeRepository.findById(request.getId()).orElseThrow(RuntimeException::new)));
-        return response;
+        return employeeService.getEmployeeDetails(request);
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetAllEmployeeDetailsRequest")
     @ResponsePayload
     public GetAllEmployeeDetailsResponse findAll() {
-
-        GetAllEmployeeDetailsResponse allEmployeeDetailsResponse = new GetAllEmployeeDetailsResponse();
-        Iterable<Employee> employees = employeeRepository.findAll();
-        for (Employee employee : employees) {
-            GetEmployeeDetailsResponse employeeDetailsResponse = mapEmployeeToGetResponse(employee);
-            allEmployeeDetailsResponse.getEmployeeDetails().add(employeeDetailsResponse.getEmployeeDetails());
-        }
-
-        return allEmployeeDetailsResponse;
+        return employeeService.getGetAllEmployeeDetails();
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "CreateEmployeeDetailsRequest")
     @ResponsePayload
     public CreateEmployeeDetailsResponse save(@RequestPayload CreateEmployeeDetailsRequest request) {
-
-        EmployeeDetails employeeDetails = request.getEmployeeDetails();
-        Employee employeeToSave = employeeMapper.mapToEmployee(employeeDetails);
-
-        CreateEmployeeDetailsResponse createEmployeeDetailsResponse = new CreateEmployeeDetailsResponse();
-        EmployeePosition employeeToSavePosition = employeeToSave.getEmployeePosition();
-        if (employeeToSavePosition.isValidSalary(employeeToSave.getSalary())) {
-            employeeRepository.save(employeeToSave);
-            createEmployeeDetailsResponse.setEmployeeDetails(employeeDetails);
-            createEmployeeDetailsResponse.setMessage("New employee was created successfully");
-        } else {
-            createEmployeeDetailsResponse.setMessage(employeeToSavePosition.getNotValidMessage(employeeToSave.getSalary()));
-        }
-        return createEmployeeDetailsResponse;
+        return employeeService.saveEmployeeDetails(request);
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "UpdateEmployeeDetailsRequest")
     @ResponsePayload
     public UpdateEmployeeDetailsResponse update(@RequestPayload UpdateEmployeeDetailsRequest request) {
-        UpdateEmployeeDetailsResponse employeeDetailsResponse = null;
-        Optional<Employee> existingEmployee = this.employeeRepository.findById(request.getEmployeeDetails().getId());
-        if (existingEmployee.isEmpty()) {
-            employeeDetailsResponse = mapEmployeeToUpdateResponse(null, "Id not found");
-        }
-        if (existingEmployee.isPresent()) {
-
-            Employee employeeToUpdate = existingEmployee.orElseThrow(RuntimeException::new);
-            employeeToUpdate.setName(request.getEmployeeDetails().getName());
-            employeeToUpdate.setSalary(request.getEmployeeDetails().getSalary());
-            employeeToUpdate.setEmployeePosition(EmployeePosition.valueOf(request.getEmployeeDetails().getEmployeeDetailsPosition().value()));
-
-            EmployeePosition employeeToUpdatePosition = employeeToUpdate.getEmployeePosition();
-            if (employeeToUpdatePosition.isValidSalary(employeeToUpdate.getSalary())) {
-                employeeRepository.save(employeeToUpdate);
-                employeeDetailsResponse = mapEmployeeToUpdateResponse(employeeToUpdate, "Updated successfully");
-            } else {
-                employeeDetailsResponse.setMessage(employeeToUpdatePosition.getNotValidMessage(employeeToUpdate.getSalary()));
-            }
-        }
-        return employeeDetailsResponse;
+        return employeeService.updateEmployeeDetails(request);
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "DeleteEmployeeDetailsRequest")
     @ResponsePayload
     public DeleteEmployeeDetailsResponse delete(@RequestPayload DeleteEmployeeDetailsRequest request) {
-
-        employeeRepository.deleteById(request.getId());
-
-        DeleteEmployeeDetailsResponse courseDetailsResponse = new DeleteEmployeeDetailsResponse();
-        courseDetailsResponse.setMessage("Deleted Successfully");
-        return courseDetailsResponse;
-    }
-
-    private GetEmployeeDetailsResponse mapEmployeeToGetResponse(Employee employee) {
-        EmployeeDetails employeeDetails = employeeMapper.mapToEmployeeDetails(employee);
-        GetEmployeeDetailsResponse employeeDetailsResponse = new GetEmployeeDetailsResponse();
-
-        employeeDetailsResponse.setEmployeeDetails(employeeDetails);
-        return employeeDetailsResponse;
-    }
-
-    private UpdateEmployeeDetailsResponse mapEmployeeToUpdateResponse(Employee employee, String message) {
-        EmployeeDetails employeeDetails = employeeMapper.mapToEmployeeDetails(employee);
-        UpdateEmployeeDetailsResponse employeeDetailsResponse = new UpdateEmployeeDetailsResponse();
-
-        employeeDetailsResponse.setEmployeeDetails(employeeDetails);
-        employeeDetailsResponse.setMessage(message);
-        return employeeDetailsResponse;
+        return employeeService.deleteEmployeeDetails(request);
     }
 }
--- complete/src/main/java/com/example/producingwebservice/service/EmployeeService.java
+++ complete/src/main/java/com/example/producingwebservice/service/EmployeeService.java
@@ -1,9 +1,102 @@
 package com.example.producingwebservice.service;
 
+import com.example.producingwebservice.domain.Employee;
+import com.example.producingwebservice.domain.EmployeePosition;
+import com.example.producingwebservice.mapper.EmployeeMapper;
+import com.example.producingwebservice.repository.EmployeeRepository;
+import https.www_rob_com.gen.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
 
+    private final EmployeeMapper employeeMapper;
+    private final EmployeeRepository employeeRepository;
 
+    public GetEmployeeDetailsResponse getEmployeeDetails(GetEmployeeDetailsRequest request) {
+        GetEmployeeDetailsResponse response = new GetEmployeeDetailsResponse();
+        response.setEmployeeDetails(employeeMapper.mapToEmployeeDetails(employeeRepository.findById(request.getId()).orElseThrow(RuntimeException::new)));
+        return response;
+    }
+
+    public GetAllEmployeeDetailsResponse getGetAllEmployeeDetails() {
+        GetAllEmployeeDetailsResponse allEmployeeDetailsResponse = new GetAllEmployeeDetailsResponse();
+        Iterable<Employee> employees = employeeRepository.findAll();
+        for (Employee employee : employees) {
+            GetEmployeeDetailsResponse employeeDetailsResponse = mapEmployeeToGetResponse(employee);
+            allEmployeeDetailsResponse.getEmployeeDetails().add(employeeDetailsResponse.getEmployeeDetails());
+        }
+
+        return allEmployeeDetailsResponse;
+    }
+
+    public CreateEmployeeDetailsResponse saveEmployeeDetails(CreateEmployeeDetailsRequest request) {
+        EmployeeDetails employeeDetails = request.getEmployeeDetails();
+        Employee employeeToSave = employeeMapper.mapToEmployee(employeeDetails);
+
+        CreateEmployeeDetailsResponse createEmployeeDetailsResponse = new CreateEmployeeDetailsResponse();
+        EmployeePosition employeeToSavePosition = employeeToSave.getEmployeePosition();
+        if (employeeToSavePosition.isValidSalary(employeeToSave.getSalary())) {
+            employeeRepository.save(employeeToSave);
+            createEmployeeDetailsResponse.setEmployeeDetails(employeeDetails);
+            createEmployeeDetailsResponse.setMessage("New employee was created successfully");
+        } else {
+            createEmployeeDetailsResponse.setMessage(employeeToSavePosition.getNotValidMessage(employeeToSave.getSalary()));
+        }
+        return createEmployeeDetailsResponse;
+    }
+
+    public UpdateEmployeeDetailsResponse updateEmployeeDetails(UpdateEmployeeDetailsRequest request) {
+        UpdateEmployeeDetailsResponse employeeDetailsResponse = null;
+        Optional<Employee> existingEmployee = this.employeeRepository.findById(request.getEmployeeDetails().getId());
+        if (existingEmployee.isEmpty()) {
+            employeeDetailsResponse = mapEmployeeToUpdateResponse(null, "Id not found");
+        }
+        if (existingEmployee.isPresent()) {
+
+            Employee employeeToUpdate = existingEmployee.orElseThrow(RuntimeException::new);
+            employeeToUpdate.setName(request.getEmployeeDetails().getName());
+            employeeToUpdate.setSalary(request.getEmployeeDetails().getSalary());
+            employeeToUpdate.setEmployeePosition(EmployeePosition.valueOf(request.getEmployeeDetails().getEmployeeDetailsPosition().value()));
+
+            EmployeePosition employeeToUpdatePosition = employeeToUpdate.getEmployeePosition();
+            if (employeeToUpdatePosition.isValidSalary(employeeToUpdate.getSalary())) {
+                employeeRepository.save(employeeToUpdate);
+                employeeDetailsResponse = mapEmployeeToUpdateResponse(employeeToUpdate, "Updated successfully");
+            } else {
+                employeeDetailsResponse.setMessage(employeeToUpdatePosition.getNotValidMessage(employeeToUpdate.getSalary()));
+            }
+        }
+        return employeeDetailsResponse;
+    }
+
+    public DeleteEmployeeDetailsResponse deleteEmployeeDetails(DeleteEmployeeDetailsRequest request) {
+        employeeRepository.deleteById(request.getId());
+
+        DeleteEmployeeDetailsResponse courseDetailsResponse = new DeleteEmployeeDetailsResponse();
+        courseDetailsResponse.setMessage("Deleted Successfully");
+        return courseDetailsResponse;
+    }
+
+
+    private GetEmployeeDetailsResponse mapEmployeeToGetResponse(Employee employee) {
+        EmployeeDetails employeeDetails = employeeMapper.mapToEmployeeDetails(employee);
+        GetEmployeeDetailsResponse employeeDetailsResponse = new GetEmployeeDetailsResponse();
+
+        employeeDetailsResponse.setEmployeeDetails(employeeDetails);
+        return employeeDetailsResponse;
+    }
+
+    private UpdateEmployeeDetailsResponse mapEmployeeToUpdateResponse(Employee employee, String message) {
+        EmployeeDetails employeeDetails = employeeMapper.mapToEmployeeDetails(employee);
+        UpdateEmployeeDetailsResponse employeeDetailsResponse = new UpdateEmployeeDetailsResponse();
+
+        employeeDetailsResponse.setEmployeeDetails(employeeDetails);
+        employeeDetailsResponse.setMessage(message);
+        return employeeDetailsResponse;
+    }
 }
