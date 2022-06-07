fe22712f6db810ddf821b36b13954af367954af1
rimangulov <rimangulov@phoenixit.ru>
2022-06-07 14:51:19 +0500

    add MaskingPatternLayout and pattern to logback-spring.xml

---
+++ complete/src/main/java/com/example/producingwebservice/support/MaskingPatternLayout.java
@@ -0,0 +1,44 @@
+package com.example.producingwebservice.support;
+
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class MaskingPatternLayout extends PatternLayout {
+
+    public static final String DELIMITER = "|";
+    public static final char ASTERISK = '*';
+    private final List<String> maskPatterns = new ArrayList<>();
+    private Pattern multilinePattern;
+
+    public void addMaskPattern(String maskPattern) {
+        maskPatterns.add(maskPattern);
+        multilinePattern = Pattern.compile(String.join(DELIMITER, maskPatterns), Pattern.MULTILINE);
+    }
+
+    @Override
+    public String doLayout(ILoggingEvent event) {
+        return maskMessage(super.doLayout(event));
+    }
+
+    private String maskMessage(String message) {
+        if (multilinePattern == null) {
+            return message;
+        }
+        StringBuilder sb = new StringBuilder(message);
+        Matcher matcher = multilinePattern.matcher(sb);
+        while (matcher.find()) {
+            IntStream.rangeClosed(1, matcher.groupCount()).forEach(group -> {
+                if (matcher.group(group) != null) {
+                    IntStream.range(matcher.start(group), matcher.end(group)).forEach(i -> sb.setCharAt(i, ASTERISK));
+                }
+            });
+        }
+        return sb.toString();
+    }
+}
