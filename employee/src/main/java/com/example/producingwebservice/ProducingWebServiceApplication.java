d986c2b16f9d5ef13bdc1794b6660f1b799e3c56
rimangulov <rimangulov@phoenixit.ru>
2022-07-01 11:45:03 +0500

    add modules EUREKA and BALANCE

--- employee/src/main/java/com/example/producingwebservice/ProducingWebServiceApplication.java
+++ employee/src/main/java/com/example/producingwebservice/ProducingWebServiceApplication.java
@@ -3,7 +3,6 @@ package com.example.producingwebservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@EnableScheduling
