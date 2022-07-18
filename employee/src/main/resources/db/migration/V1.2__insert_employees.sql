313769d92aab5bf7a2bb73a85b6572b42dcb3a93
rimangulov <rimangulov@phoenixit.ru>
2022-07-18 13:33:39 +0500

    fix insert sql script

--- employee/src/main/resources/db/migration/V1.2__insert_employees.sql
+++ employee/src/main/resources/db/migration/V1.2__insert_employees.sql
@@ -31,7 +31,7 @@ INSERT INTO employee_db.task (id, description) VALUES (14, 'fourteenth task');
 INSERT INTO employee_db.task (id, description) VALUES (15, 'fifteenth task');
 
 -- insert data in the bridge table
-INSERT INTO employee_db.employee_tasks (employee_id, task_id) VALUES (1 , 1),
+INSERT INTO employee_db.employee_tasks (employee_id, task_id) VALUES (1 , 1);
 INSERT INTO employee_db.employee_tasks (employee_id, task_id) VALUES (1 , 2);
 INSERT INTO employee_db.employee_tasks (employee_id, task_id) VALUES (1 , 3);
 INSERT INTO employee_db.employee_tasks (employee_id, task_id) VALUES (1 , 4);
