70ad1f3816886b36061f5b19af3b97c237014bce
rimangulov <rimangulov@phoenixit.ru>
2022-07-19 12:49:47 +0500

    add swagger

--- employee/src/main/java/com/example/producingwebservice/config/security/WebSecurityConfig.java
+++ employee/src/main/java/com/example/producingwebservice/config/security/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                     .authorizeRequests()
-                    .antMatchers("/login", "/registration")
+                    .antMatchers("/login", "/registration", "/swagger-ui/")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
