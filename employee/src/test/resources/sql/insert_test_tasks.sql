fb2980972dc792c30ef1c9d760e9e3f16093f16c
rimangulov <rimangulov@phoenixit.ru>
2022-07-28 16:44:08 +0500

    remove comments

--- employee/src/test/resources/sql/insert_test_tasks.sql
+++ employee/src/test/resources/sql/insert_test_tasks.sql
@@ -2,6 +2,5 @@
 INSERT INTO employee_db.task (description) VALUES ('first test task');
 INSERT INTO employee_db.task (description) VALUES ('second test task');
 INSERT INTO employee_db.task (description) VALUES ('third test task');
--- INSERT INTO employee_db.task (description) VALUES ('fourth test task');
 
 SELECT setval('employee_db.task_task_id_seq', (SELECT MAX(task_id) FROM employee_db.task))
