4e99eb22aa75a1a91efa95c636c418d7891c53d8
rimangulov <rimangulov@phoenixit.ru>
2022-07-28 18:25:37 +0500

    refactoring

--- employee/src/test/java/com/example/producingwebservice/checker/EmployeeCheckerTest.java
+++ employee/src/test/java/com/example/producingwebservice/checker/EmployeeCheckerTest.java
@@ -1,21 +1,15 @@
 package com.example.producingwebservice.checker;
 
-import com.example.producingwebservice.entity.Task;
-import com.example.producingwebservice.model.EmployeeDto;
 import com.example.producingwebservice.service.MessageService;
 import com.example.producingwebservice.service.validator.EmployeeNotValidMessageService;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
-import java.util.Set;
-
 import static com.example.producingwebservice.testData.EmployeeTestData.*;
-import static com.example.producingwebservice.testData.TaskTestData.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class EmployeeCheckerTest {
-
     private final EmployeeChecker employeeChecker = new EmployeeChecker(
             new EmployeeNotValidMessageService(
                     new MessageService(
@@ -25,52 +19,56 @@ class EmployeeCheckerTest {
 
     @Test
     void shouldCheckSalaryByPosition() {
-        EmployeeDto employeeDtoForSave = getEmployeeDtoForSaveInController();
-        employeeDtoForSave.setSalary(ONE_MILLION_SALARY);
-        String checkMessage = employeeChecker.checkSalaryByPosition(employeeDtoForSave);
-        assertThat(checkMessage).isNotNull().isEqualTo("validation.position.salary");
+        String checkMessage = employeeChecker.checkSalaryByPosition(getEmployeeDtoWithBigSalary());
+        assertThat(checkMessage)
+                .isNotNull()
+                .isEqualTo("validation.position.salary");
     }
 
     @Test
     void shouldCheckCountOfTasks() {
-        EmployeeDto employeeDtoForSave = getEmployeeDtoForSaveInController();
-        employeeDtoForSave.setTasks(Set.of(
-                modelMapper.map(getFirstTask(), Task.class),
-                modelMapper.map(getSecondTask(), Task.class),
-                modelMapper.map(getThirdTask(), Task.class)));
-        String checkMessage = employeeChecker.checkCountOfTasks(employeeDtoForSave);
-        assertThat(checkMessage).isNotNull().isEqualTo("validation.position.tasks");
+        String checkMessage = employeeChecker.checkCountOfTasks(getEmployeeDtoWithManyTasks());
+        assertThat(checkMessage)
+                .isNotNull()
+                .isEqualTo("validation.position.tasks");
     }
 
     @Test
     void shouldCheckPositionByNull() {
-        EmployeeDto employeeDtoForSave = getEmployeeDtoForSaveInController();
-        employeeDtoForSave.setPosition(null);
-        String checkMessage = employeeChecker.checkPositionByNull(employeeDtoForSave);
-        assertThat(checkMessage).isNotNull().isEqualTo("position validation.not.null");
+        String checkMessage = employeeChecker.checkPositionByNull(getEmployeeDtoWithNullPosition());
+        assertThat(checkMessage)
+                .isNotNull()
+                .isEqualTo("position validation.not.null");
     }
 
     @Test
     void shouldCheckNameLength() {
-        EmployeeDto employeeDtoForSave = getEmployeeDtoForSaveInController();
-        employeeDtoForSave.setName(SHORT_NAME);
-        String checkMessage = employeeChecker.checkNameLength(employeeDtoForSave);
-        assertThat(checkMessage).isNotNull().isEqualTo("validation.name.length");
+        String checkMessage = employeeChecker.checkNameLength(getEmployeeDtoWithShortName());
+        assertThat(checkMessage)
+                .isNotNull()
+                .isEqualTo("validation.name.length");
     }
 
     @Test
-    void shouldCheckSalary() {
-        EmployeeDto employeeDtoForSave = getEmployeeDtoForSaveInController();
-        employeeDtoForSave.setSalary(ZERO_SALARY);
-        String checkMessage = employeeChecker.checkSalary(employeeDtoForSave);
-        assertThat(checkMessage).isNotNull().isEqualTo("validation.salary.size");
+    void shouldCheckZeroSalary() {
+        String checkMessage = employeeChecker.checkSalary(getEmployeeDtoWithZeroSalary());
+        assertThat(checkMessage)
+                .isNotNull()
+                .isEqualTo("validation.salary.size");
     }
 
     @Test
     void shouldCheckNameByNull() {
-        EmployeeDto employeeDtoForSave = getEmployeeDtoForSaveInController();
-        employeeDtoForSave.setName(null);
-        String checkMessage = employeeChecker.checkNameByNull(employeeDtoForSave);
-        assertThat(checkMessage).isNotNull().isEqualTo("name validation.not.null");
+        String checkMessage = employeeChecker.checkNameByNull(getEmployeeDtoWithNullName());
+        assertThat(checkMessage)
+                .isNotNull()
+                .isEqualTo("name validation.not.null");
+    }
+
+    void shouldCheckLongName() {
+        String checkMessage = employeeChecker.checkNameByNull(getEmployeeDtoWithLongName());
+        assertThat(checkMessage)
+                .isNotNull()
+                .isEqualTo("validation.name.length");
     }
 }
--- employee/src/test/java/com/example/producingwebservice/testData/EmployeeTestData.java
+++ employee/src/test/java/com/example/producingwebservice/testData/EmployeeTestData.java
@@ -1,10 +1,14 @@
 package com.example.producingwebservice.testData;
 
+import com.example.producingwebservice.entity.Task;
 import com.example.producingwebservice.model.EmployeeDto;
+import org.modelmapper.ModelMapper;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
+import static com.example.producingwebservice.testData.TaskTestData.*;
 import static com.example.producingwebservice.type.Position.*;
 
 public class EmployeeTestData {
@@ -13,11 +17,14 @@ public class EmployeeTestData {
     public static final String SHORT_NAME = "ab";
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String APPLICATION_PDF = "application/pdf";
+    private static final String LONG_NAME = "Ahmed Ibn Hamid All Rasul Fatih Ali Babaevich Ibn Fadlan";
     public static final int ZERO_INDEX = 0;
     public static final String LOW_SALARY = "40000";
     public static final String SALARY = "51000";
     public static final String NAME = "Ivan";
     public static final long TASK_ID = 1L;
+    private static final ModelMapper modelMapper = new ModelMapper();
+
     public static EmployeeDto getFirstEmployeeDto() {
         return new EmployeeDto(null,
                 "firstUUID",
@@ -90,15 +97,6 @@ public class EmployeeTestData {
                 Collections.emptySet());
     }
 
-    public static EmployeeDto getEmployeeDtoForSaveInController() {
-        return new EmployeeDto(null,
-                "controllerSaveUUID",
-                "test employee for save in controller",
-                "55000",
-                WORKER,
-                Collections.emptySet());
-    }
-
     public static List<EmployeeDto> getEmployeeDtosForSaveInController() {
         return List.of(
                 new EmployeeDto(null,
@@ -129,5 +127,71 @@ public class EmployeeTestData {
                         "55000",
                         WORKER,
                         Collections.emptySet()));
+    }
+
+    public static EmployeeDto getEmployeeDtoWithBigSalary() {
+        return new EmployeeDto(null,
+                "bigSalaryUUID",
+                "test employee for check big salary",
+                ONE_MILLION_SALARY,
+                WORKER,
+                Collections.emptySet());
+    }
+
+    public static EmployeeDto getEmployeeDtoWithManyTasks() {
+        return new EmployeeDto(null,
+                "manyTasksUUID",
+                "test employee with many tasks",
+                "55000",
+                WORKER,
+                Set.of(
+                        modelMapper.map(getFirstTask(), Task.class),
+                        modelMapper.map(getSecondTask(), Task.class),
+                        modelMapper.map(getThirdTask(), Task.class)));
+    }
+
+    public static EmployeeDto getEmployeeDtoWithNullPosition() {
+        return new EmployeeDto(null,
+                "nullPositionUUID",
+                "test employee with null position",
+                "55000",
+                null,
+                Collections.emptySet());
+    }
+
+    public static EmployeeDto getEmployeeDtoWithShortName() {
+        return new EmployeeDto(null,
+                "shortNameUUID",
+                SHORT_NAME,
+                "55000",
+                WORKER,
+                Collections.emptySet());
+    }
+
+    public static EmployeeDto getEmployeeDtoWithZeroSalary() {
+        return new EmployeeDto(null,
+                "zeroSalaryUUID",
+                "test employee with zero salary",
+                ZERO_SALARY,
+                WORKER,
+                Collections.emptySet());
+    }
+
+    public static EmployeeDto getEmployeeDtoWithNullName() {
+        return new EmployeeDto(null,
+                "nullNameUUID",
+                null,
+                "55000",
+                WORKER,
+                Collections.emptySet());
+    }
+
+    public static EmployeeDto getEmployeeDtoWithLongName() {
+        return new EmployeeDto(null,
+                "nullNameUUID",
+                LONG_NAME,
+                "55000",
+                WORKER,
+                Collections.emptySet());
     }
 }
