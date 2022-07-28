94bb7c2065a49031a6e089a81dab8767679ad89d
rimangulov <rimangulov@phoenixit.ru>
2022-07-28 16:41:27 +0500

    refactoring

--- employee/src/test/java/com/example/producingwebservice/testData/EmployeeTestData.java
+++ employee/src/test/java/com/example/producingwebservice/testData/EmployeeTestData.java
@@ -121,6 +121,4 @@ public class EmployeeTestData {
                         WORKER,
                         Collections.emptySet()));
     }
-
-
 }
--- employee/src/test/java/com/example/producingwebservice/testData/TaskTestData.java
+++ employee/src/test/java/com/example/producingwebservice/testData/TaskTestData.java
@@ -9,9 +9,11 @@ public class TaskTestData {
     public static TaskDto getFirstTask() {
         return new TaskDto(null, "first test task");
     }
+
     public static TaskDto getSecondTask() {
         return new TaskDto(null, "second test task");
     }
+
     public static TaskDto getThirdTask() {
         return new TaskDto(null, "third test task");
     }
@@ -21,7 +23,8 @@ public class TaskTestData {
     }
 
     public static List<TaskDto> getTaskDtoList() {
-        return List.of(new TaskDto(null, "fifth test task"),
+        return List.of(
+                new TaskDto(null, "fifth test task"),
                 new TaskDto(null, "sixth test task"));
     }
 }
